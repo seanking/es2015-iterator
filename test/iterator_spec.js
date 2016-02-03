46de2562b60be3a770d4f388b7914f5717f0d3c8
Sean King <r.sean.king@gmail.com>
2016-02-03 18:22:53 -0500

    Update to assert values instead of logging to the console

--- test/iterator_spec.js
+++ test/iterator_spec.js
@@ -1,70 +1,103 @@
 'use strict';
 
-// const assert = require('chai').assert;
+const assert = require('chai').assert;
 
 describe('ES2015 Iterators ', () => {
   it('should iterate over an array', () => {
-    const arr = [1, 2, 3];
+    // Given
+    const expectedArr = [1, 2, 3];
+    const actualArr = [];
 
-    for (const i of arr) {
-      console.log(i);
+    // When
+    for (const i of expectedArr) {
+      actualArr.push(i);
     }
+
+    // Then
+    assert.deepEqual(actualArr, expectedArr);
   });
 
   it('should iterate over a string', () => {
-    const str = 'test';
+    // Given
+    const expectedString = 'test';
+    let actualStr = '';
 
-    for (const i of str) {
-      console.log(i);
+    // When
+    for (const i of expectedString) {
+      actualStr += i;
     }
+
+    // Then
+    assert.equal(actualStr, expectedString);
   });
 
   it('should iterate over a map', () => {
-    const map = new Map([['one', 1], ['two', 2]]);
+    // Given
+    const expectedMap = new Map([['one', 1], ['two', 2]]);
+    const actualMap = new Map();
 
-    for (const i of map) {
-      console.log(i);
+    // When
+    for (const i of expectedMap) {
+      actualMap.set(i);
     }
+
+    // Then
+    assert.deepEqual(actualMap, expectedMap);
   });
 
   it('should iterate over a set', () => {
-    const set = new Set([1, 2]);
+    // Given
+    const expectedSet = new Set([1, 2]);
+    const actualSet = new Set();
 
-    for (const i of set) {
-      console.log(i);
+    // When
+    for (const i of expectedSet) {
+      actualSet.add(i);
     }
+
+    // Then
+    assert.deepEqual(actualSet, expectedSet);
   });
 
   it('should print iterator contents', () => {
+    // Given
     const set = new Set([1, 2, 3]);
 
+    // When
     const iterator = set[Symbol.iterator]();
 
-    console.log(iterator.next());
-    console.log(iterator.next());
-    console.log(iterator.next());
-    console.log(iterator.next());
+    // Then
+    assert.deepEqual(iterator.next(), { value: 1, done: false });
+    assert.deepEqual(iterator.next(), { value: 2, done: false });
+    assert.deepEqual(iterator.next(), { value: 3, done: false });
+    assert.deepEqual(iterator.next(), { value: undefined, done: true });
   });
 
 
-  it('should iterate over over custom iterable', () => {
-    const uut = new CustomIterableClass();
+  it('should iterate over over custom aggregate', () => {
+    // Given
+    const agg = new Aggregate();
+    const values = [];
 
-    for (const i of uut) {
-      console.log(i);
+    // When
+    for (const i of agg) {
+      values.push(i);
     }
+
+    // Then
+    assert.deepEqual(values, agg.values);
   });
 });
 
-class CustomIterableClass {
+class Aggregate {
   constructor() {
     this.values = ['one', 'two', 'three'];
   }
 
-  [Symbol.iterator]() {
+  [Symbol.iterator]() { // implements iterable protocol
     let index = 0;
     return {
-      next: () => {
+      next: () => { // implements iterator protocol
         const value = this.values[index];
         const done = index >= this.values.length;
         index++;
